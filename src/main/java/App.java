d2d51ef08718555e996d6a12cdbbe2e55b733cd4
jumaBrian <njengabrianj@gmail.com>
2022-05-02 19:22:01 +0300

    success: local host render

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
-
+import Models.Squad;
+import Models.Hero;
 
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -13,11 +14,15 @@ public class App {
 
     public static void main(String[] args) {
 
+        staticFileLocation("/public");
+
         // Route to our index/home page
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "first-view.hbs");
         }, new HandlebarsTemplateEngine());
+
+
 
 
     }
